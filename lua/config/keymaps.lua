785655e2bc04b1308c611f4079ec8fd3bc331479
tom_bra2 <hello@brandworks.dev>
2025-03-11 14:36:13 +0100

    Init fzf,todo,project-nvim,which-key, keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,8 @@
-vim.keymap.set("n", "<C-b>", vim.cmd.Ex, {desc="Ex"})
-vim.keymap.set("i", "<C-s>", "<cmd>w<CR>")
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
+vim.keymap.set("n", "<C-b>", vim.cmd.Ex, {desc="Back"})
+vim.keymap.set("i", "<C-s>", "<cmd>w<CR>",{desc="Save"})
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>",{desc="Save"})
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>",{desc="Quit NVIM"})
+vim.keymap.set("i", "<C-q>", "<cmd>q<CR>",{desc="Quit NVIM"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
