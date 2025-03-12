0c13bca5502414d2ffd6fe10d906beccf3235720
tom_bra2 <hello@brandworks.dev>
2025-03-12 16:12:17 +0100

    keymaps options which key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,8 @@ vim.keymap.set("n", "<C-q>", "<cmd>q<CR>",{desc="Quit NVIM"})
 vim.keymap.set("i", "<C-q>", "<cmd>q<CR>",{desc="Quit NVIM"})
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+  
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,38 +1,52 @@
+
+-- Aktiviert Nerd Font Unterstützung
 vim.g.have_nerd_font = true
 
-vim.opt.langmap = "+]ü["
-vim.keymap.set("n", "ü", "[", {remap =true})
+-- Anzeigeeinstellungen
+vim.opt.number = true            -- Zeilennummern anzeigen
+vim.opt.relativenumber = true    -- Relative Zeilennummern aktivieren
+vim.opt.list = true              -- Nicht druckbare Zeichen anzeigen
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- Darstellung nicht druckbarer Zeichen
+vim.opt.signcolumn = "yes"       -- Zeichen-Spalte immer anzeigen
+vim.opt.showmode = false         -- Modus nicht in der Statuszeile anzeigen (da bereits vorhanden)
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+-- Suchverhalten
+vim.opt.ignorecase = true        -- Suche ist standardmäßig nicht zwischen Groß- und Kleinschreibung unterscheidend
+vim.opt.smartcase = true         -- Falls ein Großbuchstabe in der Suche, wird die Suche groß-/kleinschreibungssensitiv
+vim.opt.hlsearch = false         -- Suchbegriffe nicht dauerhaft hervorheben
+vim.opt.incsearch = true         -- Inkrementelle Suche aktivieren
 
--- Make line numbers default
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Einrückungen und Tabulatoren
+vim.opt.tabstop = 4              -- Tabulatorweite auf 4 setzen
+vim.opt.softtabstop = 4          -- Softtabulatorweite auf 4 setzen
+vim.opt.shiftwidth = 4           -- Einrückungsweite auf 4 setzen
+vim.opt.expandtab = true         -- Tabulatoren durch Leerzeichen ersetzen
+vim.opt.autoindent = true        -- Automatische Einrückung aktivieren
+vim.opt.smartindent = true       -- Intelligente Einrückung aktivieren
+vim.opt.smarttab = true          -- Intelligentes Tabulatorverhalten aktivieren
+vim.opt.breakindent = true       -- Zeilenumbrüche einrücken
+
+-- Verhalten bei Splits
+vim.opt.splitright = true        -- Neue vertikale Splits rechts öffnen
+vim.opt.splitbelow = true        -- Neue horizontale Splits unten öffnen
+
+-- Sonstige Optionen
+vim.opt.mouse = 'a'              -- Maussteuerung überall aktivieren
+vim.opt.undofile = true          -- Änderungen in einer Undo-Datei speichern
+vim.opt.scrolloff = 8           -- Mindestens 10 Zeilen über/unter dem Cursor anzeigen
+
+-- Zwischenablage synchronisieren
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus' -- Systemzwischenablage verwenden
+end)
 
 
--- Configure how new splits should be opened
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent =true
-vim.opt.smartindent = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.undofile =true
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff =10
+-- Nach kopieren wird ein kurzes Highlighten des Textes angezeigt
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = "Highlight yank",
+})
